f35642ff57304cdc2278f35886bc2148400c78f2
mrgroomy <konakona0106@gmail.com>
2023-02-21 22:26:52 +0900

    [Docs] README 수정

---
+++ Memo_TCA/Memo_TCA/Memo_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memo_TCAApp.swift
+//  Memo_TCA
+//
+//  Created by 박성민 on 2023/02/21.
+//
+
+import SwiftUI
+
+@main
+struct Memo_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Memo_TCA
+---
+#라이센스
+Memo_TCA is available under the MIT license. See the LICENSE file for more info.
+- [swift-composable-architecture](https://github.com/pointfreeco/swift-composable-architecture)
+
